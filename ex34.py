6f5a0e67f257162c354bd12264f9b6a7ab0cc557
LiuJLin <Liu_JLin@outlook.com>
2017-04-19 19:13:30 +0800

    Access elements of lists   version_correct

--- ex34.py
+++ ex34.py
@@ -2,6 +2,7 @@
 #How to access the elements of a list.
 animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 
+print("Please input a number (0~5).")
 num_cardinal = int(input())
 print("The animal at %d:"% num_cardinal)
 while(not(num_cardinal >= 0 and num_cardinal <= 5)):
@@ -10,6 +11,7 @@ while(not(num_cardinal >= 0 and num_cardinal <= 5)):
     print("The animal at %d\n>>"% num_cardinal)
 print(animals[num_cardinal])
 
+print("Please input a number (1~6).")
 num_ordinal = int(input())
 print("The No.%d animal:"% num_ordinal)
 while(not(num_cardinal >= 1 and num_cardinal <= 6)):
